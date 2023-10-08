f46a51aff6311a8d26fa6e7d85d90a19c2cc5b8b
Abdalla <abdotlos60@gmail.com>
2023-10-08 15:33:00 +0200

    f

--- src/compenent/home/HomeLanding.jsx
+++ src/compenent/home/HomeLanding.jsx
@@ -31,7 +31,7 @@ const pagination = {
     <Swiper
     direction={'vertical'}
     pagination={pagination}
-    mousewheel={true}
+    // mousewheel={true}
 
     modules={[Mousewheel,Pagination]}
     className="home_swiper"
