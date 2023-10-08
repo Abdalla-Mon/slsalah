73b637411b20304895242f5ea858bfba63da9827
AbdallaMon <abdotlos60@gmail.com>
2023-10-08 12:29:45 +0200

    init

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import "./App.css";
+import { RouterProvider } from "react-router-dom";
+const router = createHashRouter(
+  createRoutesFromElements(
+    <>
+      <Route path="" element={<Routes />}>
+        <Route index element={<p>home</p>} />
+        <Route path="shop" element={<p>shop</p>} />
+      </Route>
+    </>
+  )
+);
+function App() {
+  return <RouterProvider router={router}></RouterProvider>;
+}
+function Routes() {
+  const [loader, setLoader] = useState(true);
+
+  const dispatch = useDispatch();
+  useEffect(() => {
+    setLoader(false);
+  }, []);
+  return (
+    <>
+      {loader ? (
+        "loading"
+      ) : (
+        <>
+          {/* <Navbar /> */}
+          <Outlet />
+          {/* <Footer /> */}
+        </>
+      )}
+    </>
+  );
+}
+export default App;
